2cb64e2580b8f65db3468fad90c5656827580d71
alemica97 <alessandro.micalizzi08@gmail.com>
2022-02-22 16:53:02 +0100

    Ho creato una funzione che chiede all'utente 5 numeri e li pusha dentro un array vuoto, questa funzione verrà invocata 30secondi dopo la chiusura dell'alert con setTimeout

--- js/app.js
+++ js/app.js
@@ -1,10 +1,26 @@
 //Far visualizzare con un alert 5 numeri casuali
-const randomNumber = [];
+const randomNumbers = [];
 //inserisco un numero random nell'array finche la sua lunghezza è < 5
 do{
-    let numberToPush = Math.floor(Math.random() * 100);
-    console.log(numberToPush);
-    randomNumber.push(numberToPush);
-}while(randomNumber.length < 5);
+    let numbersToPush = Math.floor(Math.random() * 100);
+    console.log(numbersToPush);
+    randomNumbers.push(numbersToPush);
+}while(randomNumbers.length < 5);
 //stampo i 5 numeri in pagina con un alert come da richiesta
-alert(randomNumber);
+alert(randomNumbers);
+
+
+//dichiaro un array che verrà riempito con i numeri inseriti dall'utente
+const userNumbers = [];
+//adesso faccio partire una funzione che chiede i numeri all'utente, con un ritardo di 30sec.
+function askNumbers(){
+    do{
+        let userNumbersToPush = parseInt(prompt('Vediamo se ti ricordi i 5 numeri che ti ho mostrato:'));
+        console.log(userNumbersToPush);
+        userNumbers.push(userNumbersToPush);
+    }while(userNumbers.length < 5);
+    console.log(userNumbers);
+};
+//uso un timeout per far invocare la funzione in ritardo
+setTimeout(askNumbers, 30000);
+
