94a6987dc63e13276a22b99955ab6d42d20420b5
alemica97 <alessandro.micalizzi08@gmail.com>
2022-02-23 17:10:51 +0100

    Problema nei controlli

--- js/app.js
+++ js/app.js
@@ -1,5 +1,10 @@
 //Far visualizzare con un alert 5 numeri casuali
 const randomNumbers = [];
+//dichiaro un array che verrà riempito con i numeri inseriti dall'utente
+const userNumbers = [];
+const correctNumbers = [];
+let time = 30;
+let clock;
 //inserisco un numero random nell'array finche la sua lunghezza è < 5
 do{
     let numbersToPush = Math.floor(Math.random() * 100);
@@ -16,7 +21,7 @@ do{
 in quest'ultimo i numeri random generati dal ciclo */
 const displayRandomNumbers = document.querySelector('.display-random-numbers');
 console.log(displayRandomNumbers);
-displayRandomNumbers.innerHTML = randomNumbers;
+displayRandomNumbers.innerHTML = randomNumbers.join(' , ');
 //mi salvo il bottone dal DOM
 const startButton = document.querySelector('.btn');
 console.log(startButton);
@@ -32,16 +37,14 @@ const inputNumber2 = document.getElementById('number2');
 const inputNumber3 = document.getElementById('number3');
 const inputNumber4 = document.getElementById('number4');
 const inputNumber5 = document.getElementById('number5');
+//Salvo in una variabile l'elemento che serve a stampare il risultato
+const result = document.querySelector('.result');
+//Salvo il container per poter modificare classe e far cambiare colore in caso di vittoria o perdita
+const numbersContainer = document.querySelector('.numbers');
 
 const inputAction = document.querySelector('.input-action');
 //salvo il bottone che inserisce gli input
 const inputActionButton = document.querySelector('.input-btn');
-
-//dichiaro un array che verrà riempito con i numeri inseriti dall'utente
-const userNumbers = [];
-const correctNumbers = [];
-let time = 30;
-let clock;
 //cliccando sul bottone evoco la funzione start che a sua volta evoca le timing function
 startButton.addEventListener('click',start);
 
@@ -59,8 +62,8 @@ function start(){
 };
 
 function askNumbers(){
-    inputActionButton.addEventListener('click',addUserNumber);
     inputAction.classList.add('active');
+    inputActionButton.addEventListener('click',addUserNumber);
 };
 
 function secondsInterval(){
@@ -75,42 +78,43 @@ function secondsInterval(){
 
 function addUserNumber(){
     //prendo i valori degli input e li inserisco in un array
-    const input1 = inputNumber1.value;
-    const input2 = inputNumber2.value;
-    const input3 = inputNumber3.value;
-    const input4 = inputNumber4.value;
-    const input5 = inputNumber5.value;
-
-    userNumbers.push(input1);
-    userNumbers.push(input2);
-    userNumbers.push(input3);
-    userNumbers.push(input4);
-    userNumbers.push(input5);
-    // do{
-    //     if(!userNumbers.includes(input1,input2,input3,input4,input5) && 
-    //     !isNaN(input1,input2,input3,input4,input5)){
-    //         userNumbers.push(input1,input2,input3,input4,input5);
-    //     }else{
-    //         alert(`Forse hai già inserito ${input1}, oppure il numero non è valido poiché non è un numero o perché è maggiore di 100`);
-    //     }        
-    // }while(userNumbers.length < 5);
+    const input1 = parseInt(inputNumber1.value);
+    const input2 = parseInt(inputNumber2.value);
+    const input3 = parseInt(inputNumber3.value);
+    const input4 = parseInt(inputNumber4.value);
+    const input5 = parseInt(inputNumber5.value);
+    //Faccio un ciclo che riempie l'array dell'utente e fa dei controlli
+    do{
+        if(!userNumbers.includes(input1,input2,input3,input4,input5) && 
+        !isNaN(input1,input2,input3,input4,input5)){
+            userNumbers.push(input1,input2,input3,input4,input5);
+        }else{
+            alert(`Forse hai già inserito uno di questi numeri, oppure il numero non è valido poiché non è un numero o perché è maggiore di 100`);
+        }        
+    }while(userNumbers.length < 5);
     console.log(userNumbers);
+
     //eseguo un ciclo per controllare quali numeri inseriti sono corretti
-    for(let i = 0; i < userNumbers.length; i++){
-        console.log(userNumbers[i]);
-        console.log(randomNumbers);
-        console.log(correctNumbers);
-        if(randomNumbers.includes(userNumbers[i])){
-            correctNumbers.push(userNumbers[i]);
+    randomNumbers.forEach((number) => {
+        if(userNumbers.includes(number)){
+            correctNumbers.push(number);
+            console.log(number);
         }
-    };
-    // randomNumbers.forEach((number) => {
-    //     if(userNumbers.includes(number)){
-    //         correctNumbers.push(number);
-    //         console.log(number);
-    //     }
-    // });
-    
-    console.log('Hai indovinato questi numeri:', correctNumbers);
+    });
     inputActionButton.removeEventListener('click',addUserNumber);
+
+    inputAction.classList.remove('active');
+    result.classList.add('active');
+
+    if(correctNumbers.length === randomNumbers.length){
+        result.innerHTML = `Hai indovinato tutti i numeri !!`
+        numbersContainer.classList.add('green');
+    }else if(correctNumbers.length > 0 && correctNumbers.length < randomNumbers.length){
+        result.innerHTML = `Hai indovinato solo questi numeri: 
+                                ${correctNumbers.join(', ')} !!`
+        numbersContainer.classList.add('red');
+    }else{
+        result.innerHTML = `Non hai indovinato nessun numero !!`
+        numbersContainer.classList.add('red');
+    }
 }
